0c0081d4aeb23d3b37a018e7ae01306e5b93d1e5
youngsoohan <amazingmobdev@gmail.com>
2019-11-09 04:08:58 +0900

    fix 404 paragrph

--- src/pages/404.js
+++ src/pages/404.js
@@ -13,7 +13,10 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <Head title="404: Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          만약 찾는 자료가 없다면 url이 변경되어서 그러니 zero's 이모저모를
+          눌러주세요^^
+        </p>
       </Layout>
     )
   }
